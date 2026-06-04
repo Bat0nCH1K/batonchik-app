2a9fc632242e61dc84a91746358ac90fd012c85c
BatonCHIK <anas2013tatar@gmail.com>
2026-06-04 18:55:55 +0300

    Update highscores.js

--- highscores.js
+++ highscores.js
@@ -1,4 +1,4 @@
-// Система рекордов BatonCHIK Games v6.0
+// Система рекордов BatonCHIK Games v7.0
 const Highscores = {
     keys: {
         clicker: 'hs_clicker', quiz: 'hs_quiz', game2048: 'hs_2048',
@@ -7,6 +7,7 @@ const Highscores = {
         brickbreaker: 'hs_brickbreaker', tictactoe: 'hs_tictactoe',
         match3: 'hs_match3', race: 'hs_race', tetris: 'hs_tetris',
         memory: 'hs_memory', puzzle: 'hs_puzzle',
+        blockblast: 'hs_blockblast', fruitmerge: 'hs_fruitmerge', paperio: 'hs_paperio',
     },
     get(game) { return parseInt(localStorage.getItem(this.keys[game]) || '0'); },
     save(game, score) {
