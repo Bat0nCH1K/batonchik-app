4f6f0713321111cf94522243455d20abfcedaf38
BatonCHIK <anas2013tatar@gmail.com>
2026-06-08 08:45:51 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,37 @@
+const CACHE = 'batonchik-v8';
+const ASSETS = [
+  '/batonchik-app/',
+  '/batonchik-app/index.html',
+  '/batonchik-app/main.html',
+  '/batonchik-app/titles.html',
+  '/batonchik-app/sfx.js',
+  '/batonchik-app/highscores.js',
+  '/batonchik-app/modals.js',
+  '/batonchik-app/manifest.json',
+  '/batonchik-app/clicker.html',
+  '/batonchik-app/quiz.html',
+  '/batonchik-app/game2048.html',
+  '/batonchik-app/snake.html',
+  '/batonchik-app/flappy.html',
+  '/batonchik-app/invaders.html',
+  '/batonchik-app/minesweeper.html',
+  '/batonchik-app/pacman.html',
+  '/batonchik-app/brickbreaker.html',
+  '/batonchik-app/tictactoe.html',
+  '/batonchik-app/match3.html',
+  '/batonchik-app/race.html',
+  '/batonchik-app/tetris.html',
+  '/batonchik-app/memory.html',
+  '/batonchik-app/puzzle.html',
+  '/batonchik-app/blockblast.html',
+  '/batonchik-app/fruitmerge.html',
+  '/batonchik-app/paperio.html'
+];
+
 self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
@@ -7,5 +40,12 @@ self.addEventListener('activate', (e) => {
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      return caches.open(CACHE).then(cache => {
+        cache.put(e.request, res.clone());
+        return res;
+      });
+    }))
+  );
 });
