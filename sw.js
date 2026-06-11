c607842a8b1db8ff48970c073626312d8519192f
BatonCHIK <anas2013tatar@gmail.com>
2026-06-11 07:45:39 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,8 @@
-const CACHE = 'app-v2';
-
-self.addEventListener('install', e => {
+self.addEventListener('install', () => {
     self.skipWaiting();
 });
 
-self.addEventListener('activate', e => e.waitUntil(clients.claim()));
-
-self.addEventListener('fetch', e => {
-    e.respondWith(
-        caches.match(e.request).then(cached => {
-            if (cached) return cached;
-            return fetch(e.request).then(response => {
-                if (!response || response.status !== 200) return response;
-                const clone = response.clone();
-                caches.open(CACHE).then(c => c.put(e.request, clone));
-                return response;
-            }).catch(() => caches.match('/index.html'));
-        })
-    );
+self.addEventListener('activate', () => {
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))));
+    self.registration.unregister();
 });
